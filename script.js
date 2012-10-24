8e30b6ca2fc3a9c965dbe5e312b362cfe3a8ebc3
Barry McCardel <barrald1213@gmail.com>
2012-10-24 13:51:00 -0500

    New List Add functionality
    Has placeholders for fields, clears values upon entering and inserted
    plus signs to left

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function addEaten() {
 
+	// Gets name and note values and sets them to variables
 	var newEatenName = document.addToEaten.newEatenName.value;
-
 	var newEatenNote = document.addToEaten.newEatenNote.value;
-		
+	
+	// Creates new li with values	
 	var newEatenItem = '<li> <a href="#" class="placeName">' + newEatenName + '</a> <span class = "note">' + newEatenNote + '</span> </li>';
     
+    // Inserts new item into ul
     document.getElementById('eatenList').innerHTML = document.getElementById('eatenList').innerHTML + newEatenItem;
 
-
+    // Clears inputs
+    document.addToEaten.newEatenName.value = "";
+    document.addToEaten.newEatenNote.value = "";
 }
 
 function addUneaten() {
@@ -21,13 +25,16 @@ function addUneaten() {
     
     document.getElementById('uneatenList').innerHTML = document.getElementById('uneatenList').innerHTML + newUneatenItem;
 
-
+	// Clears inputs
+    document.addToUneaten.newUneatenName.value = "";
+    document.addToUneaten.newUneatenNote.value = "";
 
 }
 
 function alertTest() {
 	alert ("Hello World!");
 }
+
 
 
 function turnOffNeighborhood () {
