d5584776da7a55c5f4e0a0fbbe0568846edd28c0
aditkumar <aditkumar@gmail.com>
2012-11-22 16:12:43 -0500

    Hooking up backend

--- app.rb
+++ app.rb
@@ -1,6 +1,35 @@
 require 'sinatra'
 require 'pg'
 
-get '/list' do 	
+def getListName(listid)
+	conn = PG.connect(:host => 'localhost', :dbname => 'balist', :user => 'akumar', :password => nil)
+	res = conn.exec('SELECT listname, lefthand, righthand from balist.lists where listid=$1', [listid])
+	conn.close
+	if res.num_tuples == 1
+		return res[0]
+	else
+		return nil
+	end
+end
+
+def getListItems(listid, lefthand)
+	conn = PG.connect(:host => 'localhost', :dbname => 'balist', :user => 'akumar', :password => nil)
+	res = conn.exec('SELECT * from balist.listitems where listid=$1 and lefthand=$2', [listid,lefthand])
+	conn.close
+
+	if res.num_tuples > 0
+		return res
+	else
+		return nil
+	end
+end
+
+get '/' do 	
 	erb :index
+end
+
+get '/list' do 
+	listInfo = getListName(1)
+	state = {:listName => listInfo['listname'], :lListName => listInfo['lefthand'], :rListName => listInfo['righthand'], :leftList => getListItems(1,true) , :rightList => getListItems(1,false) }
+	erb :list, :locals => { :listState => state}
 end
