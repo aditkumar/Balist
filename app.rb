c21e2efe1df112bc0fa9fc8ad1938eddff42ce17
aditkumar <aditkumar@gmail.com>
2012-11-22 18:47:16 -0500

    Added Chicago list

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
 require 'pg'
 
+def openSql()
+	conn = PG.connect(:host => 'localhost', :dbname => 'balist', :user => 'akumar', :password => nil)	
+	return conn
+end
+
 def getListInfo(listid)
-	conn = PG.connect(:host => 'localhost', :dbname => 'balist', :user => 'akumar', :password => nil)
+	conn = openSql()
 	res = conn.exec('SELECT listname, lefthand, righthand from balist.lists where listid=$1', [listid])
 	conn.close
 	if res.num_tuples == 1
@@ -13,7 +18,7 @@ def getListInfo(listid)
 end
 
 def getListItems(listid, lefthand)
-	conn = PG.connect(:host => 'localhost', :dbname => 'balist', :user => 'akumar', :password => nil)
+	conn = openSql()
 	res = conn.exec('SELECT * from balist.listitems where listid=$1 and lefthand=$2', [listid,lefthand])
 	conn.close
 
@@ -25,7 +30,7 @@ def getListItems(listid, lefthand)
 end
 
 def getLists(uid)
-	conn = PG.connect(:host => 'localhost', :dbname => 'balist', :user => 'akumar', :password => nil)
+	conn = openSql()
 	res = conn.exec('SELECT listid, listname from balist.lists where uid=$1 order by listname asc', [uid])
 	conn.close
 	if res.num_tuples > 0
@@ -42,6 +47,6 @@ end
 get '/list/:listid' do 
 	allLists = getLists(2)
 	listInfo = getListInfo(params[:listid])
-	state = {:listName => listInfo['listname'], :lListName => listInfo['lefthand'], :rListName => listInfo['righthand'], :leftList => getListItems(1,true) , :rightList => getListItems(1,false) }
+	state = {:listName => listInfo['listname'], :lListName => listInfo['lefthand'], :rListName => listInfo['righthand'], :leftList => getListItems(params[:listid],true) , :rightList => getListItems(params[:listid],false) }
 	erb :list, :locals => { :listState => state, :allLists => allLists}
 end
